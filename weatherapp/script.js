ad6cbfa568e7ce63b618da9d95d715e7f28d62ca
Nandhini <150242225+nandhiniannika@users.noreply.github.com>
2024-06-18 10:33:36 +0530

    Add files via upload

---
+++ weatherapp/script.js
@@ -0,0 +1,44 @@
+const options = {
+    method: 'Get',
+    headers:{
+        'x-rapidapi-key': '1d41ad25e0msh7ed3c670d54af9fp13113ejsnd6ed91fe8e9b',
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+const getWeather = (city) =>{
+cityName.innerHTML = city
+// const getWeather2 = (city) =>{
+// Botsan.innerHTML = city
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city= ' + city,options)
+.then(response => response.json())
+ .then((response) => {
+
+    
+    console.log(response)
+   
+
+temp.innerHTML=response.temp
+temp2.innerHTML=response.temp
+feels_like.innerHTML=response.feels_like
+humidity.innerHTML=response.humidity 
+humidity2.innerHTML=response.humidity 
+min_temp.innerHTML=response.min_temp 
+max_temp.innerHTML=response.max_temp 
+wind_speed.innerHTML=response.wind_speed
+wind_speed2.innerHTML=response.wind_speed
+wind_degrees.innerHTML=response.wind_degrees
+sunrise.innerHTML=response.sunrise
+sunset.innerHTML=response.sunset
+ }
+)
+
+ .catch(err => console.error(err));
+}
+
+
+    submit.addEventListener("click",(e)=>{
+        e.preventDefault()
+        getWeather(city.value)
+    })
+getWeather("Delhi")
+
